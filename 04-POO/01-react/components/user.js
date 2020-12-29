8fb8ee459e885a588c85b015d526d63847a1cff2
Leonardo Omaña <leotangram@gmail.com>
2020-12-29 11:16:56 -0500

    Recreating React

---
+++ 04-POO/01-react/components/user.js
@@ -0,0 +1,17 @@
+import { Component } from '../lib/react.js'
+
+class User extends Component {
+  render() {
+    const { avatar, name } = this.props
+    return `
+      <div class="user">
+        <div class="avatar">
+          <img src="${avatar}" alt="Ash image"/>
+        </div>
+        <h2>${name}</h2>
+      </div>
+    `
+  }
+}
+
+export default User
