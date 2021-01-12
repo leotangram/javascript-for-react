6f5734e6f3cb3177c719daaf6002399b7748290e
Leonardo Omaña <leotangram@gmail.com>
2021-01-12 15:13:12 -0500

    create-react-element

---
+++ 04-POO/02-create-react-element/components/app.js
@@ -0,0 +1,44 @@
+import { Component, createElement } from '../lib/react/index.js'
+import User from './user.js'
+import Wrapper from './wrapper.js'
+// import UserStyled from './user-styled.js'
+
+const element = createElement(
+  'h1',
+  {
+    class: 'title'
+  },
+  'Hola mundo desde create element!'
+)
+console.log(element)
+
+class App extends Component {
+  render() {
+    return createElement('div', {
+      <div class="app">	      class: 'app',
+        <h1>hola mundo!</h1>	      children: new Wrapper({
+      </div>	        children: [
+      `	          new User({
+            name: 'Ash',
+            avatar: './images/ash.jpg'
+          })
+        ]
+      })
+    })
+  }
+}
+
+// ${new Wrapper({
+//   children: `
+//     <h1>React.js ⭐️⭐️⭐️⭐️⭐️</h1>
+//     ${new User({
+//       name: 'Ash',
+//       avatar: './images/ash.jpg'
+//     }).render()}
+//     ${new UserStyled({
+//       name: 'Ash',
+//       avatar: './images/ash.jpg'
+//     }).render()}
+//   `
+// }).render()}
+export default App
